51437bb97b37d6cdd723c690b6dbfed6f53f7996
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-03-20 21:18:15 -0400

    add scenarios for animal feeding

---
+++ modules/design/spec/features/animal_feeding_spec.rb
@@ -0,0 +1,15 @@
+RSpec.feature 'Animal feeding' do
+  scenario 'feeding a cat some dog food'
+  scenario 'feeding a cat some cat food'
+  scenario 'feeding a cat some chicken'
+  scenario 'feeding a cat some milk'
+  scenario 'feeding a cat some lemon'
+  scenario 'feeding a cat some human food'
+
+  scenario 'feeding a dog some dog food'
+  scenario 'feeding a dog some cat food'
+  scenario 'feeding a dog some chicken'
+  scenario 'feeding a dog some milk'
+  scenario 'feeding a dog some lemon'
+  scenario 'feeding a dog some human food'
+end
