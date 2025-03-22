805048ceb46508cde363c5d826a3c8566b5b401e
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-03-22 15:23:00 -0400

    refactoring spec
    - split the one big spec in two.
    - utilized a table over divs in order to compare data better.

--- modules/database/spec/features/provider_management_spec.rb
+++ modules/database/spec/features/provider_management_spec.rb
@@ -1,79 +1,90 @@
 require 'rails_helper'
 
-RSpec.feature 'provider management' do
-  scenario 'finding clients of a provider' do
-    # client setup
+RSpec.feature 'Client management' do
+  scenario 'creating clients, providers, and associating them' do
     clients = %w(Sal Why Zod)
-    clients.each do |name|
-      email = "#{name}@localhost".downcase
+    clients.each {|x| create_client(x) }
 
-      visit!('/database/clients/new')
-      fill_in("Name", with: name)
-      fill_in("Email", with: email)
-
-      click_button
-
-      expect(current_path).to match(/clients\/\d+\z/)
-      expect(page).to have_text(name)
-      expect(page).to have_text(email)
-    end
-
-    # provider setup
     providers = %w(Bob Jill Jon)
-    providers.each.with_index do |name|
-      email = "#{name}@localhost".downcase
-
-      visit!('/database/providers/new')
-      fill_in("Name", with: name)
-      fill_in("Email", with: email)
-
-      click_button
-
-      expect(current_path).to match(/providers\/\d+\z/)
-      expect(page).to have_text(name)
-      expect(page).to have_text(email)
-    end
+    providers.each {|x| create_provider(x) }
 
     # add relationships and check that they are visible
     clients.each.with_index do |client, i|
-      visit!('/database/clients')
-
-      click_on(client)
-
-      click_on('Edit')
-
       sub_providers = providers[0..i]
-      sub_providers.each do |provider|
-        check(provider)
-      end
+      associate_client_to_providers(client, sub_providers)
+    end
+  end
 
-      click_button
+  scenario 'changing the plan' do
+    provider = "Bob".tap {|x| create_provider(x) }
+    client = "Steven".tap {|x| create_client(x) }
+    associate_client_to_providers(client, [provider])
 
-      expect(current_path).to match(/clients\/\d+\z/)
+    visit("/database/clients")
+    click_on client
 
-      providers[i+1..-1].each do |unchecked_provider|
-        expect(page).to_not have_text(unchecked_provider)
-      end
+    click_on provider
+    expect(current_path).to match(/clients\/\d+\/providers\/\d+\z/)
+    click_on 'Make Premium'
+    click_on client
+    within("##{provider}") { expect(page).to have_text("Premium") }
 
-      # Check that all subproviders exist
-      sub_providers.each do |provider|
-        click_on provider
-        expect(current_path).to match(/clients\/\d+\/providers\/\d+\z/)
-        click_on client
-      end
+    click_on provider
+    click_on 'Make Basic'
+    click_on client
+    within("##{provider}") { expect(page).to have_text("Basic") }
+  end
 
-      # run through premium functionality
-      premium_provider = sub_providers.first
-      click_on premium_provider
+  def create_client(name)
+    email = "#{name}@localhost".downcase
+
+    visit!('/database/clients/new')
+    fill_in("Name", with: name)
+    fill_in("Email", with: email)
+
+    click_button
+
+    expect(current_path).to match(/clients\/\d+\z/)
+    expect(page).to have_text(name)
+    expect(page).to have_text(email)
+  end
+
+  def create_provider(name)
+    email = "#{name}@localhost".downcase
+
+    visit!('/database/providers/new')
+    fill_in("Name", with: name)
+    fill_in("Email", with: email)
+
+    click_button
+
+    expect(current_path).to match(/providers\/\d+\z/)
+    expect(page).to have_text(name)
+    expect(page).to have_text(email)
+  end
+
+  def associate_client_to_providers(client, sub_providers)
+    visit!('/database/clients')
+
+    click_on(client)
+
+    click_on('Edit')
+
+    sub_providers.each do |provider|
+      check(provider)
+    end
+
+    click_button
+
+    expect(current_path).to match(/clients\/\d+\z/)
+
+    expect(table_hash.tap{|x| binding.irb}.map{|row| row['Name']}.sort).to eq(sub_providers.sort)
+
+    # Check that all subproviders are visible
+    sub_providers.each do |provider|
+      click_on provider
       expect(current_path).to match(/clients\/\d+\/providers\/\d+\z/)
-      click_on 'Make Premium'
       click_on client
-      within("##{premium_provider}") { expect(page).to have_text("Premium") }
-
-      click_on premium_provider
-      click_on 'Make Basic'
-      click_on client
-      within("##{premium_provider}") { expect(page).to have_text("Basic") }
     end
   end
 end
