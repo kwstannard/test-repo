8bfa3030d959fa60ade03cfdd4ba6cda9064e2cb
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-03-24 15:23:48 -0400

    add items and lists
    - each item starts at Todo and can be dragged between lists.

--- modules/react/spec/features/todo_list_spec.rb
+++ modules/react/spec/features/todo_list_spec.rb
@@ -2,11 +2,35 @@ require 'rails_helper'
 
 RSpec.feature 'TODO list app' do
   scenario 'adding an item to the list' do
-    visit!('/react/todo')
-    expect(page).to have_content("ToDo")
-    expect(page).to have_content("Doing")
-    expect(page).to have_content("Done")
+    page.driver.with_playwright_page {|p|
+      logs = []
+      errors = []
+      p.on('console', ->(msg) { logs << msg })
+      p.on('weberror', ->(msg) { errors << msg })
 
-    click_on("New Item")
+      visit!('/react/todo')
+      expect(page).to have_content("ToDo")
+      expect(page).to have_content("Doing")
+      expect(page).to have_content("Done")
+
+      # add items
+      fill_in("Text", with: "Do the dishes")
+      click_button
+      within("#ToDo") { expect(page).to have_content("Do the dishes") }
+
+      fill_in("Text", with: "Wash the car")
+      click_button
+      within("#ToDo") { expect(page).to have_content("Wash the car") }
+
+      fill_in("Text", with: "Feed the cat")
+      click_button
+      within("#ToDo") { expect(page).to have_content("Feed the cat") }
+
+      # moving items across lists
+      find("li", text: "Do the dishes")
+        .drag_to(find("#Doing"))
+      within("#Doing") { expect(page).to have_content("Do the dishes") }
+      within("#ToDo") { expect(page).to_not have_content("Do the dishes") }
+    }
   end
 end
--- modules/react/src/app.tsx
+++ modules/react/src/app.tsx
@@ -1,11 +1,84 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HTML5Backend } from "react-dnd-html5-backend"
+import { DndProvider, useDrag, useDrop } from "react-dnd"
 
 const App = () => {
+  const [todos, setTodos] = React.useState([]);
+  const [doings, setDoings] = React.useState([]);
+  const [dones, setDones] = React.useState([]);
+
+  const addToToDo = addItemFunc(setTodos);
+  const addToDoing = addItemFunc(setDoings);
+  const addToDone = addItemFunc(setDones);
+
+  const cutFromToDo = cutItemFunc(setTodos);
+  const cutFromDoing = cutItemFunc(setDoings);
+  const cutFromDone = cutItemFunc(setDones);
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <List name="ToDo" items={todos} addItem={addToToDo} cutItem={cutFromToDo} />
+      <List name="Doing" items={doings} addItem={addToDoing} cutItem={cutFromDoing} />
+      <List name="Done" items={dones} addItem={addToDone} cutItem={cutFromDone} />
+
+      <NewItemForm addItem={addToToDo}/>
+    </DndProvider>
+  )
+};
+
+const addItemFunc = (setList) => {
+  return (item) => {
+    setList((prevList) => [
+      ...prevList,
+      item
+    ]);
+  }
+};
+
+const cutItemFunc = (setList) => {
+  return (item) => {
+    setList((prevList) => prevList.filter((i) => (console.log(i, item) && i !== item )));
+  }
+};
+
+const List = ({name, items, addItem, cutItem}) => {
+  const [{isOver, canDrop }, drop] = useDrop(() => ({
+    accept: "Item",
+    drop: addItem
+  }));
+
   return(
-    <table>
-      <tr><th>ToDo</th><th>Doing</th><th>Done</th></tr>
-    </table>
+    <div id={name} ref={drop}>
+      <h1>{name}</h1>
+      <ul>
+        { items.map((item) => <Item item={item} liftFunc={cutItem} /> )}
+      </ul>
+    </div>
+  );
+}
+
+const Item = ({item, liftFunc}) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "Item",
+    item: item,
+    end: liftFunc
+  }));
+  return(<li key={item.key} ref={drag}>{item.text}</li>)
+};
+
+const NewItemForm = ({addItem}) => {
+  const newItem = (event) => {
+    event.preventDefault();
+    addItem({text: event.target.itemText.value, key: event.target.itemText.value});
+    event.target.reset();
+  };
+  return(
+    <form id="#newItemForm" onSubmit={newItem}>
+      <label htmlFor="itemText">Text</label>
+      <input type="text" name="itemText" id="itemText" placeholder="Clean" />
+      <button><span>Submit</span></button>
+    </form>
   )
 };
 
