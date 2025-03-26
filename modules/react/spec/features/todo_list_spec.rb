f322164e6801dbf99a85d37e0189dcf0eabe60df
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-03-26 00:03:53 -0400

    finishing touches
    - Celebrate button was picked because I ran into difficulties with
      trying to differentiate the Done column after spending the whole
      exercise building the lists as fully identical. It spins things around
      a few times.
    
    - add a bit of styling to not look too terrible.
    
    - Figured out how to call the add/cut/insert functions such that they
      happen in the correct order when drag and drop happens.

--- modules/react/spec/features/todo_list_spec.rb
+++ modules/react/spec/features/todo_list_spec.rb
@@ -15,22 +15,62 @@ RSpec.feature 'TODO list app' do
 
       # add items
       fill_in("Text", with: "Do the dishes")
-      click_button
-      within("#ToDo") { expect(page).to have_content("Do the dishes") }
-
+      click_button("Submit")
       fill_in("Text", with: "Wash the car")
-      click_button
-      within("#ToDo") { expect(page).to have_content("Wash the car") }
-
+      click_button("Submit")
       fill_in("Text", with: "Feed the cat")
-      click_button
-      within("#ToDo") { expect(page).to have_content("Feed the cat") }
+      click_button("Submit")
+
+      within("#ToDo") { expect(page.text).to eq(<<~DOC.chomp) }
+        ToDo
+        Do the dishes
+        Wash the car
+        Feed the cat
+      DOC
 
       # moving items across lists
-      find("li", text: "Do the dishes")
-        .drag_to(find("#Doing"))
+      find(".item", text: "Do the dishes")
+        .drag_to(find("#Doing > h1"))
       within("#Doing") { expect(page).to have_content("Do the dishes") }
       within("#ToDo") { expect(page).to_not have_content("Do the dishes") }
+
+      find(".item", text: "Do the dishes")
+        .drag_to(find("#ToDo > h1"))
+      within("#Doing") { expect(page).to_not have_content("Do the dishes") }
+      within("#ToDo") { expect(page).to have_content("Do the dishes") }
+
+      # moving items around a list
+      within("#ToDo") { expect(page.text).to eq(<<~DOC.chomp) }
+        ToDo
+        Wash the car
+        Feed the cat
+        Do the dishes
+      DOC
+
+      find(".item", text: "Do the dishes")
+        .drag_to(find(".item", text: "Feed the cat"))
+
+      within("#ToDo") { expect(page.text).to eq(<<~DOC.chomp) }
+        ToDo
+        Wash the car
+        Do the dishes
+        Feed the cat
+      DOC
+
+      find(".item", text: "Do the dishes")
+        .drag_to(find(".item", text: "Wash the car"))
+
+      within("#ToDo") { expect(page.text).to eq(<<~DOC.chomp) }
+        ToDo
+        Do the dishes
+        Wash the car
+        Feed the cat
+      DOC
+
+      # Done pizzazz
+
+      find(".item", text: "Do the dishes")
+        .drag_to(find("#Done"))
     }
   end
 end
--- modules/react/src/app.tsx
+++ modules/react/src/app.tsx
@@ -4,67 +4,103 @@ import { HTML5Backend } from "react-dnd-html5-backend"
 import { DndProvider, useDrag, useDrop } from "react-dnd"
 
 const App = () => {
+  const orig_todos=[
+    {text: 'Do the dishes', key: 'Do the dishes'},
+    {text: 'Do the lawn', key: 'Do the lawn'},
+    {text: 'Do the monster mash', key: 'Do the monster mash'},
+    {text: 'Do the do', key: 'Do the do'},
+  ]
+
+  // const [todos, setTodos] = React.useState(orig_todos);
   const [todos, setTodos] = React.useState([]);
   const [doings, setDoings] = React.useState([]);
   const [dones, setDones] = React.useState([]);
+
+  console.log('todo', todos);
+  console.log('doin', doings);
+  console.log('done', dones);
+
+  const cutItemAtToDo = cutItemAtFunc(setTodos);
+  const cutItemAtDoing = cutItemAtFunc(setDoings);
+  const cutItemAtDone = cutItemAtFunc(setDones);
 
   const addToToDo = addItemFunc(setTodos);
   const addToDoing = addItemFunc(setDoings);
   const addToDone = addItemFunc(setDones);
 
-  const cutFromToDo = cutItemFunc(setTodos);
-  const cutFromDoing = cutItemFunc(setDoings);
-  const cutFromDone = cutItemFunc(setDones);
+  const insertAtToDo = insertItemAtFunc(setTodos);
+  const insertAtDoing = insertItemAtFunc(setDoings);
+  const insertAtDone = insertItemAtFunc(setDones);
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <List name="ToDo" items={todos} addItem={addToToDo} cutItem={cutFromToDo} />
-      <List name="Doing" items={doings} addItem={addToDoing} cutItem={cutFromDoing} />
-      <List name="Done" items={dones} addItem={addToDone} cutItem={cutFromDone} />
+      <List name="ToDo" items={todos} addItem={addToToDo} cutItemAt={cutItemAtToDo} insertItemAt={insertAtToDo} />
+      <List name="Doing" items={doings} addItem={addToDoing} cutItemAt={cutItemAtDoing} insertItemAt={insertAtDoing} />
+      <List name="Done" items={dones} addItem={addToDone} cutItemAt={cutItemAtDone} insertItemAt={insertAtDone} />
 
       <NewItemForm addItem={addToToDo}/>
+      <button onClick={pizzazz}>Celebrate!</button>
     </DndProvider>
   )
 };
 
-const addItemFunc = (setList) => {
-  return (item) => {
-    setList((prevList) => [
-      ...prevList,
-      item
-    ]);
-  }
-};
+const pizzazzStyle = document.createElement("style")
+document.head.appendChild(pizzazzStyle)
 
-const cutItemFunc = (setList) => {
-  return (item) => {
-    setList((prevList) => prevList.filter((i) => (console.log(i, item) && i !== item )));
-  }
-};
+const pizzazz = () => {
+  var deg = 0;
+  iid=setInterval(
+    () => { deg++; pizzazzStyle.textContent = "#root { transform: rotate("+deg+"deg) }" },
+    1
+  )
 
-const List = ({name, items, addItem, cutItem}) => {
-  const [{isOver, canDrop }, drop] = useDrop(() => ({
+  setTimeout(
+    () => { clearInterval(iid); pizzazzStyle.textContent = "" },
+    5000
+  )
+}
+
+const addItemFunc = (setList) =>
+  (item) => setList((prevList) => [...prevList,item]) && {result: true};
+
+const insertItemAtFunc = (setList) =>
+  (index) =>
+    (item, monitor) =>
+      setList((prevList) => [...prevList.slice(0,index), item, ...prevList.slice(index)])
+
+const cutItemAtFunc = (setList) =>
+  (index) =>
+    (item, monitor) =>
+      setList((prevList) => prevList.splice(index, 1) && prevList)
+
+const List = ({name, items, addItem, cutItemAt, insertItemAt}) => {
+  const [{isOver, canDrop }, drop] = useDrop({
     accept: "Item",
-    drop: addItem
-  }));
+    drop: (i,m) => { i.cut(); addItem({...i, new: true},m) }
+  });
 
   return(
-    <div id={name} ref={drop}>
-      <h1>{name}</h1>
-      <ul>
-        { items.map((item) => <Item item={item} liftFunc={cutItem} /> )}
-      </ul>
+    <div id={name}>
+      <h1 ref={drop}>{name}</h1>
+      <div class="list">
+        { items.map((item, index) => <Item item={item} cutItem={cutItemAt(index)} insertItem={insertItemAt(index)} /> )}
+      </div>
     </div>
   );
 }
 
-const Item = ({item, liftFunc}) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
+const Item = ({item, cutItem, insertItem}) => {
+  const [{ isDragging }, drag] = useDrag({
     type: "Item",
-    item: item,
-    end: liftFunc
-  }));
-  return(<li key={item.key} ref={drag}>{item.text}</li>)
+    item: { ...item, cut: cutItem }
+  });
+  const [{isOver, canDrop }, drop] = useDrop({
+    accept: "Item",
+    drop: (i,m) => { i.cut(); insertItem(i,m) }
+  });
+  return(<div class="item" key={item.key} ref={drag}>
+           <div ref={drop}>{item.text}</div>
+         </div>)
 };
 
 const NewItemForm = ({addItem}) => {
@@ -76,7 +112,7 @@ const NewItemForm = ({addItem}) => {
   return(
     <form id="#newItemForm" onSubmit={newItem}>
       <label htmlFor="itemText">Text</label>
-      <input type="text" name="itemText" id="itemText" placeholder="Clean" />
+      <input type="text" name="itemText" id="itemText" placeholder="Clean placeholders" />
       <button><span>Submit</span></button>
     </form>
   )
